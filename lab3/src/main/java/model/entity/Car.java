8737d8e0d5a8f5797c63c8368b51f656820b0f50
Marcus Svensson <msvenss@student.chalmers.se>
2020-02-12 09:20:21 +0100

    Added lab3

---
+++ lab3/src/main/java/model/entity/Car.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Spondon
+ */
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car implements Serializable {
+    @Id private String license;
+    private String name;
+    
+}
---
+++ lab3/target/generated-sources/annotations/model/entity/QCar_.java
@@ -0,0 +1,18 @@
+package model.entity;
+
+import easycriteria.meta.*;
+
+public class QCar_ extends ObjectAttribute<Car>{
+    public QCar_() {
+        this(null, null);
+    }
+    public StringPropertyAttribute<Car, java.lang.String> license = new StringPropertyAttribute<>("license", this, java.lang.String.class);
+    public StringPropertyAttribute<Car, java.lang.String> name = new StringPropertyAttribute<>("name", this, java.lang.String.class);
+    private static InstancesInits inits = InstancesInits.DIRECT2;
+    public QCar_(String attribute, EntityPathNode parent) {
+        this(attribute, parent, inits);
+    }
+    public QCar_(String attribute, EntityPathNode parent, InstancesInits inits) {
+        super(attribute, parent, Car.class);
+    }
+}
