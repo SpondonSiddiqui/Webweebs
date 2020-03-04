da7a430558fdde8c049881f743b6046e2e8bf570
Spondon Siddiqui <spondonsiddiqui@gmail.com>
2020-03-04 10:18:18 +0100

    Added a backing bean for login with some typical variables and methods

---
+++ Labb 3/lab3/src/main/java/model/view/LoginBackingBean.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.view;
+
+/**
+ *
+ * @author Spondon
+ */
+public class LoginBackingBean {
+
+    private String username;
+    private String password;
+    private boolean isUsernameValid ;
+    private boolean isPasswordValid ;
+    private boolean validationComplete  = false;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean getIsUsernameValid() {
+        return isUsernameValid;
+    }
+
+    public void setUsernameValid(boolean isUsernameValid) {
+        this.isUsernameValid = isUsernameValid;
+    }
+
+    public boolean getIsPasswordValid() {
+        return isPasswordValid;
+    }
+
+    public void setPasswordValid(boolean isPasswordValid) {
+        this.isPasswordValid = isPasswordValid;
+    }
+
+    public boolean getValidationComplete() {
+        return validationComplete;
+    }
+
+    public void setValidationComplete(boolean validationComplete) {
+        this.validationComplete = validationComplete;
+    }
+
+    public String checkValidity() {
+        if (this.username == null || this.username.equals("")) {
+            isUsernameValid = false;
+        } else {
+            isUsernameValid = true;
+        }
+        if (this.password == null || this.password.equals("")) {
+            isPasswordValid = false;
+        } else {
+            isPasswordValid = true;
+        }
+        validationComplete = true;
+        return "success";
+    }
+}
