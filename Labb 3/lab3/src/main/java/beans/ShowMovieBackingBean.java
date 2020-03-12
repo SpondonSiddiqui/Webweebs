0892cef33cd5e5f34539eba1aa0b5e7ad92c60d1
Mathias <lammers@student.chalmers.se>
2020-03-12 09:51:37 +0100

    Typ klar för redovisning

--- Labb 3/lab3/src/main/java/beans/ShowMovieBackingBean.java
+++ Labb 3/lab3/src/main/java/beans/ShowMovieBackingBean.java
@@ -15,10 +15,13 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 import lombok.Data;
 import model.dao.MovieDAO;
 import model.entity.Movie;
 import org.omnifaces.cdi.Param;
+import org.omnifaces.util.Faces;
+import org.omnifaces.util.Messages;
 
 /**
  *
@@ -32,7 +35,7 @@ public class ShowMovieBackingBean implements Serializable {
     @Inject
     @Param(name = "name")
     private String name;
-    @Min(value = 10, message = "Minimum is 10 characters")
+    @Size(min = 5, max = 100, message = "Review need to be between 5 and 100 characters")
     private String review;
 
     @EJB
@@ -46,7 +49,15 @@ public class ShowMovieBackingBean implements Serializable {
 
     public void validateSubmission() {
         movie = movieDAO.findMoviesByName(name).get(0);
-        movie.setReviews(review);
-        movieDAO.update(movie);
+        if (!movie.getReviews().equals("")) {
+            movie.setReviews(review);
+            movieDAO.update(movie);
+            Messages.addGlobalWarn("Added review", null);
+            Faces.validationFailed();
+        } else {
+            Messages.addGlobalWarn("You have already written a review "
+                    + "of this movie.", null);
+            Faces.validationFailed();
+        }
     }
 }
